28832fe7df28b7aa1d6ca6a74eb1597cd7af0773
rp <pradhanr@mailfence.com>
2025-08-07 22:01:18 -0700

    Add functionality to look around with mouse

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,26 +1,33 @@
 #include "camera.hpp"
 
-Camera::Camera(glm::vec3 position) {
-    this->position = position;
-    direction = glm::vec3(0.f, 0.f, 0.f);
-    projection = glm::perspective(glm::radians(45.f), float(defaultWidth) / float(defaultHeight), 0.1f, 100.f);
+Camera::Camera(glm::vec3 position, float fov, float yaw, float pitch, float sensitivity, float zoom) {
+    m_position = position;
+    m_fov = fov;
+    m_yaw = yaw;
+    m_pitch = pitch;
+    m_sensitivity = sensitivity;
+    m_zoom = zoom;
+    m_projection = glm::perspective(glm::radians(m_fov), float(defaultWidth) / float(defaultHeight), 0.1f, 100.f);
+    updateVectors();
     updateView();
 }
 
-// glm::mat4 Camera::getView() {
-//     glm::vec3 direction = glm::normalize(position - target);
-//     glm::vec3 rightAxis = glm::normalize(glm::cross(
-//         glm::vec3(0.f, 1.f, 0.f), // up vector
-//         direction
-//     ))
-//
-//     glm::vec3 upAxis = glm::cross(direction, rightAxis);
-//
-//     return glm::lookAt(position, target, )
-// }
-//
-//
+void Camera::processMouse(double offsetX, double offsetY) {
+    offsetX *= m_sensitivity;
+    offsetY *= m_sensitivity;
 
-void Camera::updateView() {
-    view = glm::lookAt(position, position + direction + glm::vec3(0.f, 0.f, -1.f), glm::vec3(0.f, 1.f, 0.f));
+    m_yaw -= offsetX;
+    m_pitch += offsetY;
+    updateVectors();
 }
+
+void Camera::updateVectors() {
+    glm::vec3 front;
+    front.x = cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+    front.y = sin(glm::radians(m_pitch));
+    front.z = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+
+    m_front = glm::normalize(front);
+    m_rightAxis = glm::normalize(glm::cross(m_front, glm::vec3(0.f, 1.f, 0.f)));
+    m_upAxis = glm::normalize(glm::cross(m_rightAxis, m_front));
+} 
