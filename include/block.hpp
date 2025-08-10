864042de1f3903e6b366e91d79e024e929cf2142
rp <pradhanr@mailfence.com>
2025-08-09 22:13:07 -0700

    Added blocks

---
+++ include/block.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "constants.hpp"
+
+enum {
+    BLOCK_AIR,
+    BLOCK_DIRT,
+
+
+
+    NUMBER_OF_BLOCKS
+};
+
+class Block {
+public:
+    bool transparent;
+};
+
+inline Block blockArray[NUMBER_OF_BLOCKS] = {};
+
+// Hacking c++ to let me assign global variables :(
+namespace {
+    struct _BlockInitializer {
+        _BlockInitializer() {
+            blockArray[BLOCK_AIR] = {true};
+            blockArray[BLOCK_DIRT] = {false};
+        }
+    };
+
+    inline _BlockInitializer _block_initializer_instance;
+}
