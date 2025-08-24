c95b59059e51c66898f596c728b0b5c1843a220d
rp <pradhanr@mailfence.com>
2025-08-23 19:25:52 -0700

    Add updateMesh

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -1,7 +1,22 @@
 #include "chunk.hpp"
 
 void Chunk::generate() {
-    for (uint i = 0; i < m_chunkData.getLength(); i++) {
-        m_chunkData.setBlock(i, blockArray[BLOCK_DIRT]);
+    for (uint i = 0; i < m_chunkData->getLength(); i++) {
+        m_chunkData->setBlock(i, blockArray[BLOCK_DIRT]);
+    }
+}
+
+void Chunk::updateMesh() {
+    
+
+    for (uint x = 0; x < m_chunkData->getSizeX(); x++)
+    for (uint y = 0; y < m_chunkData->getSizeY(); y++)
+    for (uint z = 0; z < m_chunkData->getSizeZ(); z++) {
+        Block currBlock = m_chunkData->getBlock(x, y, z);
+        if (currBlock.isTransparent()) {
+            continue;
+        }
+
+
     }
 }
