833ca2e206eca065874dbdea674f2313057d8467
pradhan-ritik <pradhanr@mailfence.com>
2025-09-11 09:57:28 -0700

    world.cpp - Added implementation of createChunk(), generateWorld(), and render()

--- src/world.cpp
+++ src/world.cpp
@@ -1,17 +1,40 @@
 #include "world.hpp"
 
-World::World(uint renderDistance, uint chunkSizeX, uint chunkSizeY, uint chunkSizeZ) {
-    m_renderDistance = renderDistance;
-    // Render distance should be even
-    assert((m_renderDistance % 2) == 0);
-    m_chunkSizeX = chunkSizeX;
-    m_chunkSizeY = chunkSizeY;
-    m_chunkSizeZ = chunkSizeZ;
-    m_chunks = (Chunk *)malloc(siezof(Chunk) * m_renderDistance * m_renderDistance);
+void World::createChunk(uint x, uint z) {
+    Chunk* currChunk = getChunkArrayCoordinates(x, z);
+    assert(currChunk == nullptr || !currChunk->hasBeenGenerated());
+    if (currChunk == nullptr) {
+        currChunk = new Chunk(localToChunkCoords(x, z));
+    }
+
+    currChunk->generate();
+    currChunk->updateMesh();
 }
 
+void World::unloadChunk(uint x, uint z) {
+    // Chunk* currChunk = getChunkArrayCoordinates(x, z);
+    // _saveChunkData(currChunk)
+}
 
-// void World::changeRenderDistance(uint renderDistance) { 
+void World::_saveChunkData(Chunk* chunk) {
+
+}
+
+void World::generateWorld() {
+    for (uint x = 0; x < m_renderDistance; x++) {
+        for (uint z = 0; z < m_renderDistance; z++) {
+            createChunk(x, z);
+
+        }
+    }
+}
+
+void World::render(Renderer& renderer, Shader& shader, const Camera& camera) {
+    for (uint i = 0; i < (m_renderDistance * m_renderDistance); i++) {
+        renderer.draw(*(m_chunks[i]->getMesh()), shader, camera);
+    }
+}
+// void World::changeRenderDistance(uint renderDistance) {
 //     m_renderDistance = renderDistance;
 //     m_chunks = (Chunk *)malloc(siezof(Chunk) * m_renderDistance * m_renderDistance);
 // }
