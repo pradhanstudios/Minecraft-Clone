30364d037e3bc9ea1fc9d5ebe5620cd5cfc18340
pradhan-ritik <pradhanr@mailfence.com>
2025-09-11 11:13:46 -0700

    world.cpp - Fixed createChunk()

--- src/world.cpp
+++ src/world.cpp
@@ -5,6 +5,7 @@ void World::createChunk(uint x, uint z) {
     assert(currChunk == nullptr || !currChunk->hasBeenGenerated());
     if (currChunk == nullptr) {
         currChunk = new Chunk(localToChunkCoords(x, z));
+        m_chunks[arrayCoordinates(x, z)] = currChunk;
     }
 
     currChunk->generate();
@@ -24,13 +25,14 @@ void World::generateWorld() {
     for (uint x = 0; x < m_renderDistance; x++) {
         for (uint z = 0; z < m_renderDistance; z++) {
             createChunk(x, z);
-
         }
     }
 }
 
 void World::render(Renderer& renderer, Shader& shader, const Camera& camera) {
     for (uint i = 0; i < (m_renderDistance * m_renderDistance); i++) {
+        assert(m_chunks[i] != nullptr);
+        assert(m_chunks[i]->hasBeenGenerated());
         renderer.draw(*(m_chunks[i]->getMesh()), shader, camera);
     }
 }
