a3a9cc1e73a9b9c0c650d4c2e8e9120802a3ad25
pradhan-ritik <pradhanr@mailfence.com>
2025-09-11 11:11:30 -0700

    game.cpp - Implemented world into game

--- src/game.cpp
+++ src/game.cpp
@@ -3,37 +3,38 @@
 
 // Constructor
 Game::Game(uint fps)
-	: m_window(nullptr), m_renderer(nullptr), m_shader(nullptr), m_cubeMesh(nullptr), m_fps(fps) {
+	: m_window(nullptr), m_renderer(nullptr), m_shader(nullptr), m_camera(nullptr), m_world(nullptr), m_fps(fps) {
 	init();
 	std::cout << "Game initialized." << std::endl;
 }
 
 // Destructor
 Game::~Game() {
-	if (m_cubeMesh) {
-		delete m_cubeMesh;
-		m_cubeMesh = nullptr;
-	}
 	if (m_shader) {
 		delete m_shader;
 		m_shader = nullptr;
 	}
+
 	if (m_renderer) {
 		delete m_renderer;
 		m_renderer = nullptr;
 	}
+
 	if (m_window) {
 		delete m_window;
 		m_window = nullptr;
 	}
+
     if (m_camera) {
         delete m_camera;
         m_camera = nullptr;
     }
-    if (m_chunk) {
-        delete m_chunk;
-        m_chunk = nullptr;
+
+    if (m_world) {
+        delete m_world;
+        m_world = nullptr;
     }
+
 	std::cout << "Game components cleaned up." << std::endl;
 }
 
@@ -42,6 +43,8 @@ void Game::init() {
 	m_renderer = new Renderer();
 	m_shader = new Shader(vertexShaderPath, fragmentShaderPath);
     m_camera = new Camera(glm::vec3(0.f, 0.f, 10.f));
+    m_world = new World(m_camera->getPosition(), 8);
+    m_world->generateWorld();
 
     glfwSetWindowUserPointer(m_window->getGLFWwindow(), this);
     glfwSetCursorPos(m_window->getGLFWwindow(), 0, 0);
@@ -53,59 +56,6 @@ void Game::init() {
 		exit(EXIT_FAILURE);
 	}
 
-	// Triangle vertices
-	// float vertices[] = {
-	// 	-0.5f, -0.5f, 0.0f,
-	// 	 0.5f, -0.5f, 0.0f,
-	// 	 0.0f,  0.5f, 0.0f
-	// };
-
-	// m_triangleMesh = new Mesh(vertices, sizeof(vertices) / sizeof(float));
-
-
-	//    float vertices[] = {
-	//        // front
-	//        -1.0, -1.0,  1.0,
-	//         1.0, -1.0,  1.0,
-	//         1.0,  1.0,  1.0,
-	//        -1.0,  1.0,  1.0,
-	//        // back
-	//        -1.0, -1.0, -1.0,
-	//         1.0, -1.0, -1.0,
-	//         1.0,  1.0, -1.0,
-	//        -1.0,  1.0, -1.0
-	//    };
-	//
-	//    uint elements[] = {
-	// 	// front
-	// 	0, 1, 2,
-	// 	2, 3, 0,
-	// 	// right
-	// 	1, 5, 6,
-	// 	6, 2, 1,
-	// 	// back
-	// 	7, 6, 5,
-	// 	5, 4, 7,
-	// 	// left
-	// 	4, 0, 3,
-	// 	3, 7, 4,
-	// 	// bottom
-	// 	4, 5, 1,
-	// 	1, 0, 4,
-	// 	// top
-	// 	3, 2, 6,
-	// 	6, 7, 3
-	// };
-
-
-    // m_cubeMesh = new Mesh(&vertices[0], sizeof(vertices) / sizeof(float), &elements[0], sizeof(elements) / sizeof(uint));
-
-    m_chunk = new Chunk(glm::vec3(-8.f, -8.f, 0.f));
-    std::cout << "Created Chunk object" << std::endl;
-    m_chunk->generate();
-    std::cout << "Generated Chunk" << std::endl;
-    m_chunk->updateMesh();
-    std::cout << "Created Mesh for Chunk" << std::endl;
 	std::cout << "Game initialization complete." << std::endl;
 }
 
@@ -184,5 +134,5 @@ void Game::update() {
 void Game::render() {
 	m_renderer->clear();
 
-	m_renderer->draw(*m_chunk->getMesh(), *m_shader, *m_camera);
+    m_world->render(*m_renderer, *m_shader, *m_camera);
 }
